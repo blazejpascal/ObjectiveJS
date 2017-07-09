4dd5729dc23d87b58737e0bd0847e6b780999a9b
Stopczyk <you@example.com>
2017-07-09 11:44:22 +0200

    Example of class composition. Implementatnion of Singleton pattern.

--- index.js
+++ index.js
@@ -31,13 +31,48 @@ class Beam extends Bar {
     return Math.sqrt(xSquared + ySquared);
   }
 }
-class CurvedBar extends Bar {
+class CurvedBar extends Bar { // inheritance definition. curved bar inherits from bar all its method and properties. the is relation curved bar IS Bar
   constructor(id, startingNode, endingNode, curvature) {
-    super(id, startingNode, endingNode);
-    this.curvature = curvature;
+    super(id, startingNode, endingNode); // creates object of parent class
+    this.curvature = curvature; // extends parent object with new properti
   }
 }
+// Example of composition
+class Structure {
+    constructor(id, beam) {
+      this.id = id;
+      this.beams = [beam];
+    }
+    addBeam(beam) {
+      this.beams.push(beam);
+    }
+    removeBeam(beamId) {
+      var indexOfBeam;
+      for (var i=0; i < this.beams.length; i++) {
+        if (this.beams[i].id === beamId) {
+          indexOfBeam = i;
+          break;
+        }
+      }
+      if (!indexOfBeam) {
+        //console.log("Beam id not found.");
+        throw new Error("Beam id not found.");
+      }
+      this.beams.splice(indexOfBeam, 1);
+    }
+}
 
+
+// Example of Singleton Design Pattern
+var singletonInstance;
+class Singleton {
+  constructor() {
+    if (!singletonInstance) {
+      singletonInstance = this;
+    }
+    return singletonInstance;
+  }
+}
 // Tests
 function isNodeInstanciable() {
   var node = new Node(1, 0, 0);
@@ -61,8 +96,22 @@ function isConstructionAnalysysWorking() {
   var distance = ConstructionAnalysis.distance(firstNode, secondNode);
   assert.equal(distance, 5, "Nodes distance calculated incorecctly. Actual: " + distance + ", Expected: " + 5);
 }
+function isStructureWorking() {
+  var firstNode = new Node(1, 3, 8);   //new rezerwuje miejsce w pamięci na obiekt zwrócony przez wywołany konstruktor;
+  var secondNode = new Node(2, 6, 12);
+  var beam = new Beam(1, firstNode, secondNode);
+  var structure = new Structure(1, beam);
+  var thirdNode = new Node(3, 2, 6);
+  var secondBeam = new Beam(2, firstNode, thirdNode);
+
+  structure.addBeam(secondBeam); //important! going to object structure and running addBeam function
+  assert(structure.beams[1]);
+  structure.removeBeam(secondBeam.id);
+  assert(!structure.beams[1]);
+}
 
 isNodeInstanciable();
 isBeamLenghtWorking();
 isCurvedBarCurvatureDefined();
 isConstructionAnalysysWorking();
+isStructureWorking();
